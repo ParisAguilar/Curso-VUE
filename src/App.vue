1105a4ef6837fe3aa6294b12397d1c06a55ef1d6
Jóvenes Construyendo el Futuro 19 <jovenes.cf19@conacyt.mx>
2019-07-01 09:43:38 -0500

    Primer commit en cursos-Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,24 +8,6 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style >
+@import '../node_modules/bulma/bulma.sass'
 </style>
--- src/views/faq.vue
+++ src/views/faq.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    HOMEE
+  <div class="faq">
+    FAQQ
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -11,7 +11,7 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
+  name: "faq",
   components: {
     HelloWorld
   }
